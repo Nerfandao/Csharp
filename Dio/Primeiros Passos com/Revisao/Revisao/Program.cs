3993766423ef521cbbab4a08ec1376e5cc3878d9
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2023-03-06 15:09:47 -0300

    Revisao

---
+++ Dio/Primeiros Passos com/Revisao/Revisao/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Revisao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opçcaoUsuario = ObterOpcaoUsuario();
+
+            while (opçcaoUsuario.ToUpper() != "X")
+            {
+                switch (opçcaoUsuario)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do aluno");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O Valor da nota deve ser decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
+                        break;
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if (!string.IsNullOrEmpty(a.Nome))
+                            {
+                                Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota} ");
+                            }
+                        }
+
+                        break;
+                    case "3":
+                        decimal notatotal = 0;
+                        var nrAlunos = 0;
+
+                        for (int i = 0 ; i < alunos.Length; i++)
+                        {
+                            if (!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notatotal = notatotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+
+                        var mediaGeral = notatotal / nrAlunos;
+                        Console.WriteLine($"MÉDIA GERAL: {mediaGeral}");
+
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opçcaoUsuario = ObterOpcaoUsuario();
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opção desejada");
+            Console.WriteLine("1 - Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular média geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opçcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opçcaoUsuario;
+        }
+    }
+}
---
+++ Dio/Primeiros Passos com/Revisao/Revisao/conceito.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisao
+{
+    public enum conceito
+    {
+        A, B, C, D, E,
+    }
+}
