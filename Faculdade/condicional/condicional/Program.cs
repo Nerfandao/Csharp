9f4d58ea3423612c892564f62c239da17794092f
Fernando Gigliotti <100796054+Nerfandao@users.noreply.github.com>
2023-03-08 21:46:40 -0300

    aula bundinha

---
+++ Faculdade/condicional/condicional/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace condicional
+{
+    internal class Program
+    {
+        public static void parOuImpar(int num1)
+        {
+            //TODO: Arrumar essa penga aqui
+            if (num1 % 2 == 1)
+                Console.WriteLine("Acesso concedido");
+            else (num1 % 2 == 0)
+                Console.WriteLine("Comando errado");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua senha");
+            string pwd = Console.ReadLine();
+            if (pwd == "patinho22") 
+            {
+                Console.Clear();
+                Console.WriteLine("Logado com sucesso");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Digite um numero impar");
+                int num1 = int.Parse(Console.ReadLine());
+                parOuImpar(num1);
+                if (num1 == 1)
+                {
+                    Console.WriteLine("Digite um numero de 0-6");
+                    int dia = int.Parse(Console.ReadLine());
+                    switch (dia)
+                    {
+                        case 0:
+                            Console.WriteLine("Domingo");
+                            break;
+                        case 1:
+                            Console.WriteLine("Segunda");
+                            break;
+                        case 2:
+                            Console.WriteLine("Terça");
+                            break;
+                        case 3:
+                            Console.WriteLine("Quarta");
+                            break;
+                        case 4:
+                            Console.WriteLine("Quinta");
+                            break;
+                        case 5:
+                            Console.WriteLine("Sexta");
+                            break;
+                        case 6:
+                            Console.WriteLine("Sabado");
+                            break;
+                        default:
+                            Console.WriteLine("Não existe esse comando");
+                            break;
+                    }
+                }
+                else
+                    Console.WriteLine("Comando errado");
+                           
+            }
+            else
+                Console.WriteLine("Senha incorreta");
+        }
+    }
+}
---
+++ Faculdade/lacoDeRepeticao/lacoDeRepeticao/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lacoDeRepeticao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero para saber se ele é primo");
+            //int num = int.Parse(Console.ReadLine());
+
+            for (int num = 0; num < 100; num++)
+            {
+                if (num == 2 || num == 3 || num == 5 || num == 7)
+                {
+                    Console.WriteLine($"{num} É primo");
+                }
+                else if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0 || num % 7 == 0)
+                {
+                    Console.WriteLine($"{num} Não é primo");
+                }
+                else
+                {
+                    Console.WriteLine($"{num} É primo");
+                }
+            }
+        }
+    }
+}
