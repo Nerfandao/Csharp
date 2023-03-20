1cdfea0e7d05766282a4a3f3d52a3ef09b108002
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2023-03-20 15:39:33 -0300

    beecrowd

---
+++ Faculdade/Beecrowd/beecrowd_1064/beecrowd_1064/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace beecrowd_1064
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            double soma = 0;
+            
+            for (int i = 0; i  < 6 ; i++)
+            {
+                double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (num > 0 )
+                {
+                    j++;
+                    soma = num + soma;
+                }
+            }
+            soma = soma / j;
+            Console.WriteLine($"{j} valores positivos");
+            Console.WriteLine(soma.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Faculdade/Beecrowd/beecrowd_1114/beecrowd_1114/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace beecrowd_1114
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = false;
+            while (i == false)
+            {
+                int senha = int.Parse(Console.ReadLine());
+                if (senha == 2002)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    i = true;
+                }
+                else
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
+        }
+    }
+}
