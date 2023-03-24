1faf43aaa2232a9c1c65e3ae74074ba83865fed3
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2023-03-24 16:38:22 -0300

    bc

---
+++ Faculdade/Beecrowd/beecrowd_1168/beecrowd_1168/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace beecrowd_1168
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> respfinal = new List<string>();
+            int entrada = int.Parse(Console.ReadLine());
+            for (int i = 0; i < entrada; i++)
+            {
+                int led = 0;
+                string letreiro = Console.ReadLine();
+                foreach (char j in letreiro)
+                {
+                    if (j == '1')
+                        led += 2;
+                    if (j == '2' || j == '5' || j == '3')
+                        led += 5;
+                    if (j == '4')
+                        led += 4;
+                    if (j == '6' || j == '9' || j == '0')
+                        led += 6;
+                    if (j == '7')
+                        led += 3;
+                    if (j == '8')
+                        led += 7;
+                }
+                respfinal.Add($"{led} leds");
+            }
+            foreach (string i in respfinal)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
---
+++ Faculdade/Beecrowd/beecrowd_1238/beecrowd_1238/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace beecrowd_1238
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
