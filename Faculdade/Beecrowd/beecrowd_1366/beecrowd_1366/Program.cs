19734ae792e12a4d8502c606fd9a4219afb6e048
Fernando Gigliotti <pucca.bruzil@hotmail.com.br>
2023-04-07 12:34:56 -0300

    ++

---
+++ Faculdade/Beecrowd/beecrowd_1366/beecrowd_1366/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace beecrowd_1366
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                while (true)
+                {
+                    int n = int.Parse(Console.ReadLine());
+                    if (n == 0)
+                    {
+                        break;
+                    }
+                    int s = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        string[] input = Console.ReadLine().Split();
+                        int b = int.Parse(input[1]);
+                        s += b / 2;
+                    }
+                    Console.WriteLine(s / 2);
+                }
+            }
+        }
+    }
+}
